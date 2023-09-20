31d7a9246293fde4f5e7b7754e16cb3cf6288eeb
dianabogdanova <dbogda15@yandex.ru>
2023-09-20 18:47:24 +0500

    SpringBootApplication classes

---
+++ src/main/java/me/dbogda/ufanettestcoffeeshop/UfanetTestCoffeeShopApplication.java
@@ -0,0 +1,12 @@
+package me.dbogda.ufanettestcoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfanetTestCoffeeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfanetTestCoffeeShopApplication.class, args);
+    }
+}
